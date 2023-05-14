08e1e04f65f480877b02905fc38083afe8ba3ec7
Gustavo Galarza <galarzagus@outlook.com>
2023-05-14 20:46:51 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const pizzas = [
+    {
+      id: 1,
+      nombre: "pizza de Muzzarella",
+      precio: 500,
+      ingredientes: ["Muzzarella", "Tomate", "Aceitunas"],
+      imagen: "./img/muzzarella.png",
+    },
+  
+    {
+      id: 2,
+      nombre: "pizza de Cebolla",
+      precio: 1500,
+      ingredientes: ["Muzzarella", "Tomate", "Cebolla"],
+      imagen: "./img/cebolla.png",
+    },
+  
+    {
+      id: 3,
+      nombre: "pizza 4 Quesos",
+      precio: 1380,
+      ingredientes: [
+        "Muzzarella",
+        "Tomate",
+        "Queso Azul",
+        "Parmesano",
+        "Roquefort",
+      ],
+      imagen: "./img/4quesos.png",
+    },
+  
+    {
+      id: 4,
+      nombre: "pizza Especial",
+      precio: 1000,
+      ingredientes: ["Muzzarella", "Tomate", "Rucula", "Jamón"],
+      imagen: "./img/especial.png",
+    },
+  
+    {
+      id: 5,
+      nombre: "pizza con Anana",
+      precio: 600,
+      ingredientes: ["Muzzarella", "Tomate", "Anana"],
+      imagen: "./img/anana.png",
+    },
+  ];
+
+  
+    const input = document.querySelector(".imput")
+    const formulario = document.querySelector("add-form")
+    const card_comida = document.querySelector("card_comida")
+    let ids = JSON.parse(localStorage.getItem("id"))
+
+    const init = () => {
+
+        
+
+
+    };
+    init();
